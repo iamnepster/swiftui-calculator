64709f00673437534be82f7d383a2ca5b727705a
Jan Völker <jan-voelker@t-online.de>
2020-06-21 19:48:54 +0200

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Jan-Nikolas Völker on 21.06.20.
+//  Copyright © 2020 Jan-Nikolas Völker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
